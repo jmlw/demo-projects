a992ecb80bc88a19d966afb155940ef76d2a31ff
Josh Wood <josh.m.l.wood@gmail.com>
2019-04-30 07:40:54 -0500

    create minimal kubernetes config example

---
+++ spring-cloud-kubernetes-config-demo/src/test/java/com/joshmlwood/springcloudkubernetesconfigdemo/SpringCloudKubernetesConfigDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joshmlwood.springcloudkubernetesconfigdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudKubernetesConfigDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
