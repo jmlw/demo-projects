a88f5cac8afbe7cefee075b8062a81074b67da9d
Josh Wood <josh.m.l.wood@gmail.com>
2019-07-30 23:33:29 -0500

    add environment variable configuration

--- spring-cloud-kubernetes-config-demo/src/main/java/com/joshmlwood/springcloudkubernetesconfigdemo/ConfigurableController.java
+++ spring-cloud-kubernetes-config-demo/src/main/java/com/joshmlwood/springcloudkubernetesconfigdemo/ConfigurableController.java
@@ -6,19 +6,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class ConfigurableController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurableController.class);
 
     private String externalConfig;
+    private String environmentVariable;
 
-    public ConfigurableController(@Value("${app.config}") String externalConfig) {
+    public ConfigurableController(
+            @Value("${app.config}") String externalConfig,
+            @Value("${app.environmentVariable}") String environmentVariable
+    ) {
         this.externalConfig = externalConfig;
-        LOGGER.info(String.format("Current configuration is from: %s", externalConfig));
+        this.environmentVariable = environmentVariable;
+        LOGGER.info(String.format("app.config: %s\napp.environmentVariable: %s", externalConfig, environmentVariable));
     }
 
     @GetMapping("/")
-    public String getConfig() {
-        return externalConfig;
+    public Map<String, String> getConfig() {
+        Map<String, String> config = new HashMap<>();
+        config.put("app.config", externalConfig);
+        config.put("app.environmentVariable", environmentVariable);
+        return config;
     }
 }
