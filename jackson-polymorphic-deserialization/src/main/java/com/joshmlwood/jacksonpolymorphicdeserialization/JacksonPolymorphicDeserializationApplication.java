bde374a8b8e1e79350343d8a4705461743cb06c0
Josh Wood <josh.m.l.wood@gmail.com>
2020-10-08 18:59:49 -0500

    initalize project for jackson demo

---
+++ jackson-polymorphic-deserialization/src/main/java/com/joshmlwood/jacksonpolymorphicdeserialization/JacksonPolymorphicDeserializationApplication.java
@@ -0,0 +1,13 @@
+package com.joshmlwood.jacksonpolymorphicdeserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonPolymorphicDeserializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonPolymorphicDeserializationApplication.class, args);
+	}
+
+}
